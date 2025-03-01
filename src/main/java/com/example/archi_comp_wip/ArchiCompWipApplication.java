4e4d19fc68d7997d3e84354ab342a60f930b342e
Gouyon Loic <gouyonloic@MacBook-Pro-de-Gouyon.local>
2025-03-01 09:59:41 +0100

    premier push init

---
+++ src/main/java/com/example/archi_comp_wip/ArchiCompWipApplication.java
@@ -0,0 +1,13 @@
+package com.example.archi_comp_wip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiCompWipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchiCompWipApplication.class, args);
+    }
+
+}
